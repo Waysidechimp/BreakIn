fd675934169fffe54ce5a3d127d7c4f89f9ae60a
Waysidechimp <67596048+Waysidechimp@users.noreply.github.com>
2023-08-31 23:02:01 -0400

    Brick Spawning Implemented
    Added the spawning of bricks on game start. Code has adjustable sliders if needed.

---
+++ Assets/Scripts/BrickSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject brickPrefab;
+    [SerializeField] Transform startSpawn;
+    [SerializeField, Range(1, 5)] int rowNumber = 0;
+    [SerializeField, Range(1, 10)] int columnNumber = 0;
+    [SerializeField, Range(0.6f, 2)] float rowbuffer = 0;
+    [SerializeField, Range(1.7f, 2f)] float columnbuffer = 0;
+
+    Vector3 buffer = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i < rowNumber; i++)
+        {
+            for (int j=0; j < columnNumber; j++)
+            {
+                Instantiate(brickPrefab, startSpawn.position + buffer, Quaternion.identity);
+                buffer.x += columnbuffer;
+            }
+            buffer.x = 0;
+            buffer.y -= rowbuffer;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

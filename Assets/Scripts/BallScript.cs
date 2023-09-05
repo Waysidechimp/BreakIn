b05dd9c14491cd86f40677f719c2daf39994e17f
Evan Valencia <evanv.prof@gmail.com>
2023-09-05 11:44:44 -0400

    Paddle and Ball created
    Created the paddle and ball game objects as well as the scripts.
    - Ball resets.
    - Click W to send it forward.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    //If with paddle is true follow the paddle and shoot forward when
+    //player clicks
+
+    public bool withPaddle = true;
+    public GameObject paddle;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (withPaddle)
+            followPaddle();
+
+        fireBall();
+    }
+
+    void followPaddle()
+    {
+        Vector3 temp = new Vector3(paddle.transform.position.x, paddle.transform.position.y + 2, paddle.transform.position.z);
+        transform.position = temp;
+    }
+
+    void fireBall()
+    {
+        if (Input.GetKeyDown("w")){
+            withPaddle = false;
+            rb.velocity = Vector2.up * 10f;   
+        }
+    }
+
+
+
+    /// <summary>
+    /// If comes into contact with playerBase
+    /// reset volcity and relaunch the ball.
+    /// </summary>
+    /// <param name="collision"></param>
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided:" + collision.gameObject.tag);
+
+
+        if (collision.gameObject.tag == "PlayerBase")
+        {
+            rb.velocity = Vector3.zero;
+            withPaddle = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    public float speed;
+    public float maxX = 7.5f;
+
+    private float horizontalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalPosition = Input.GetAxis("Horizontal");
+        move(); 
+    }
+
+    /// <summary>
+    /// This method moves the ball left and right while also
+    /// keeping it in bounds of the screen
+    /// </summary>
+    private void move()
+    {
+        if((horizontalPosition>0 && transform.position.x < maxX) || (horizontalPosition< 0 && transform.position.x > -maxX))
+        {
+            transform.position += Vector3.right* horizontalPosition * speed * Time.deltaTime;
+        }
+    }
+
+
+}

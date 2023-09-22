c782711b877109bd36d295c8eac4b5e2642224c9
Evan Valencia <evanv.prof@gmail.com>
2023-09-22 16:45:28 -0400

    Brought back Scripts from the dead

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField]
@@ -9,11 +9,20 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] float invulnerability = 0.5f;
     private PolygonCollider2D polygon;
     [SerializeField] ParticleSystem explosion;
+    private GameObject textObject;
+    private Text deathText;
 
     // Start is called before the first frame update
     void Start()
     {
         polygon = gameObject.GetComponent<PolygonCollider2D>();
+    }
+
+    private void Awake()
+    {
+        textObject = GameObject.FindGameObjectWithTag("KillCounter");
+        deathText = textObject.GetComponent<Text>();
+
     }
 
     // Update is called once per frame
@@ -28,12 +37,19 @@ public class EnemyScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ball") {
+        if (collision.gameObject.tag == "Ball" || collision.gameObject.tag == "Paddle") {
             ParticleSystem debris = Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity);
             debris.transform.position = new Vector3(debris.transform.position.x + 0.5f, debris.transform.position.y - 0.25f, debris.transform.position.z);
-
+            addDeathCount();
             Destroy(this.gameObject);
         }
+    }
+
+    private void addDeathCount()
+    {
+
+        int currentDeaths = int.Parse(deathText.text) + 1;
+        deathText.text = currentDeaths.ToString();
     }
 
     private void iFrame()

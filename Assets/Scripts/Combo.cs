5af15e070d8d202cd56c4a24ef7ae1528802a1fb
Waysidechimp <67596048+Waysidechimp@users.noreply.github.com>
2023-10-25 20:23:22 -0400

    Added Font and Background to main UI
    Added a font to the UI and also a background to make the text pop out more

--- Assets/Scripts/Combo.cs
+++ Assets/Scripts/Combo.cs
@@ -15,6 +15,6 @@ public class Combo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = ballScript.combo + "x";
+        text.text = ballScript.combo + " X";
     }
 }
